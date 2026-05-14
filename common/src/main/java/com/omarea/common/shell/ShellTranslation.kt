e86a8c5b3e8fdcae3a8b062c4f386c4575bac7ad
MinDeaDBlood <151534222+MinDeaDBlood@users.noreply.github.com>
2026-05-14 19:52:27 +0300

    Update shell resource translation

--- common/src/main/java/com/omarea/common/shell/ShellTranslation.kt
+++ common/src/main/java/com/omarea/common/shell/ShellTranslation.kt
@@ -3,34 +3,35 @@ package com.omarea.common.shell
 import android.content.Context
 import java.util.Locale
 
-// 从Resource解析字符串，实现输出内容多语言
 class ShellTranslation(val context: Context) {
-    // 示例：
-    // @string:home_shell_01
     private val regex1 = Regex("^@(string|dimen):[_a-z]+.*", RegexOption.IGNORE_CASE)
-    // 示例
-    // @string/home_shell_01
     private val regex2 = Regex("^@(string|dimen)/[_a-z]+.*", RegexOption.IGNORE_CASE)
 
     fun resolveRow(originRow: String): String {
-        val separator = if (regex1.matches(originRow)) {
+        if (originRow.contains("\n")) {
+            return originRow.split("\n").joinToString("\n") { resolveRow(it) }
+        }
+
+        val row = originRow.trim()
+        val reference = row.substringBefore("|")
+        val separator = if (regex1.matches(reference)) {
             ':'
-        } else if (regex2.matches(originRow)) {
+        } else if (regex2.matches(reference)) {
             '/'
         } else {
             null
         }
         if (separator != null) {
-            val row = originRow.trim()
             val resources = context.resources
-            val type = row.substring(1, row.indexOf(separator)).toLowerCase(Locale.ENGLISH)
-            val name = row.substring(row.indexOf(separator) + 1)
+            val type = reference.substring(1, reference.indexOf(separator)).toLowerCase(Locale.ENGLISH)
+            val name = reference.substring(reference.indexOf(separator) + 1)
+            val args = row.split("|").drop(1).toTypedArray()
 
             try {
                 val id = resources.getIdentifier(name, type, context.packageName)
                 when (type) {
                     "string" -> {
-                        return resources.getString(id)
+                        return if (args.isEmpty()) resources.getString(id) else resources.getString(id, *args)
                     }
                     "dimen" -> {
                         return resources.getDimension(id).toString()
@@ -45,5 +46,4 @@ class ShellTranslation(val context: Context) {
 
         return originRow
     }
-
 }
